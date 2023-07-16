c852d5625999099c934c94d2ac9fc91724e98503
angelvalentinn <altieriangel1@gmail.com>
2023-07-15 21:50:49 -0300

    añadiendo imgs para hacer slider en rama_prueba

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,24 +1,32 @@
-import aside from "../assets/aside-imgs/aside-1.webp";
+import { useState, useEffect } from "react";
+import aside1 from "../assets/aside-imgs/aside-1.webp";
+import aside2 from "../assets/aside-imgs/aside-2.webp";
+import aside3 from "../assets/aside-imgs/aside-3.webp";
+import aside4 from "../assets/aside-imgs/aside-4.webp";
+import aside5 from "../assets/aside-imgs/aside-5.webp";
 import ArrowLeft from "../components/helpers/ArrowLeft";
 import ArrowRight from "../components/helpers/ArrowRight";
 
+
 const Aside = () => {
 
-    const saludar = () => {
-        console.log("hola beba");
-    }
+    const [url, setUrl] = useState(aside1);
 
+    const handleRight = () => {
+        setUrl(aside2);
+    }
+    
     return (
         <aside className="aside">
-            <img srcSet={aside} alt="Imágen de ofertas" className="aside-img"/>
-            <span onClick={saludar}><ArrowLeft clase={"aside-arrowLeft"} /></span>
-            <ArrowRight clase={"aside-arrowRight"}/>
+            <img srcSet={url} alt="Imágen de ofertas" className="aside-img"/>
+            <span ><ArrowLeft clase={"aside-arrowLeft"} /></span>
+            <span onClick={handleRight}><ArrowRight  clase={"aside-arrowRight"}/></span>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li onClick={ () => setUrl(aside1)}></li>
+                <li onClick={ () => setUrl(aside2)}></li>
+                <li onClick={ () => setUrl(aside3)}></li>
+                <li onClick={ () => setUrl(aside4)}></li>
+                <li onClick={ () => setUrl(aside5)}></li>
             </ul>
         </aside>
     )
