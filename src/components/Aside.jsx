6973994a1ce7f6986bb6eafabc0f14c43f383ede
angelvalentinn <altieriangel1@gmail.com>
2023-07-19 16:14:19 -0300

    nada importante

---
+++ src/components/Aside.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import aside1 from "../assets/aside-imgs/aside-1.webp";
+import aside2 from "../assets/aside-imgs/aside-2.webp";
+import aside3 from "../assets/aside-imgs/aside-3.webp";
+import aside4 from "../assets/aside-imgs/aside-4.webp";
+import aside5 from "../assets/aside-imgs/aside-5.webp";
+import ArrowLeft from "../components/helpers/ArrowLeft";
+import ArrowRight from "../components/helpers/ArrowRight";
+
+
+const Aside = () => {
+
+    const [url, setUrl] = useState(aside1);
+
+    const handleRight = () => {
+        if(url == aside1) setUrl(aside2)
+        else if(url == aside2) setUrl(aside3)
+        else if(url == aside3) setUrl(aside4)
+        else if(url == aside4) setUrl(aside5)
+        else if(url == aside5) setUrl(aside1)        
+    }
+    
+    return (
+        <aside className="aside">
+            <img srcSet={url} alt="Imágen de ofertas" className="aside-img"/>
+            <span ><ArrowLeft clase={"aside-arrowLeft"} /></span>
+            <span onClick={handleRight}><ArrowRight  clase={"aside-arrowRight"}/></span>
+            <ul>
+                <li onClick={ () => setUrl(aside1) }></li>
+                <li onClick={ () => setUrl(aside2) }></li>
+                <li onClick={ () => setUrl(aside3) }></li>
+                <li onClick={ () => setUrl(aside4) }></li>
+                <li onClick={ () => setUrl(aside5) }></li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Aside
