c397577f76608dd0bf193e816d5584ec47383bd3
angelvalentinn <altieriangel1@gmail.com>
2023-09-22 16:29:30 -0300

    add socialmedia

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,13 @@ const Footer = () => {
   return (
     <footer>
         <p>Ángel Altieri - Proyecto Final - Coderhouse</p>
+        <div>
+          <a href="https://instagram.com/angel_altieri_" target="_blank"><i className="bi bi-instagram"></i></a>
+          -
+          <a href="https://www.linkedin.com/in/%C3%A1ngel-valent%C3%ADn-altieri-a319a0256/" target="_blank"><i className="bi bi-linkedin"></i></a>
+          -
+          <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcSBnNMlvkDSxGMmzJCNqlZvMKfWJhRzFZcmmxcRPjkZQJmhgBQtbrBQPvdBggQFdjfkhhnGZ" target="_blank"><i className="bi bi-envelope"></i></a>
+        </div>
     </footer>
   )
 }
